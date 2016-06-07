16a3b7011c57766a0c3b8b9d390270621b9c1e0f
paulo ricardo <pauloricardojm@hotmail.com.br>
2016-06-07 18:56:43 -0300

    List implementation

---
+++ list.c
@@ -0,0 +1,19 @@
+HEAD_LIST *cria_lista(char *genero)
+{
+	
+}
+
+HEAD_LIST *insere_comeco(HEAD_LIST *comeco)
+{
+	
+}
+
+void exclui_lista(HEAD_LIST *comeco)
+{
+	
+}
+
+void insere_fim(HEAD_LIST *comeco)
+{
+	
+}
