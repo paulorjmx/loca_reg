1e11887ece80cecfd8abe1bfa1e81ae425aabc03
paulo ricardo <pauloricardojm@hotmail.com.br>
2016-06-07 18:57:26 -0300

    List header with functions prototypes

--- inc/list.h
+++ inc/list.h
@@ -2,8 +2,21 @@
     #define LIST_H
     #include "loc.h"
 
+	typedef struct list 
+	{
+		GAME *g;
+		struct list *prox;
+	}LIST;
+		
     typedef struct head_list
     {
-        
+		char genero[31];
+        LIST *ptr_list;
     } HEAD_LIST;
+	
+	
+	HEAD_LIST *cria_lista(char *genero); // Função que cria a lista;
+	HEAD_LIST *insere_comeco(HEAD_LIST *comeco); // Função para inserir um nó no começo;
+	void exclui_lista(HEAD_LIST *comeco); // Função que exclui a lista passado o começo dela;
+	void insere_fim(HEAD_LIST *comeco); // Função para inserir um nó no começo;
 #endif
