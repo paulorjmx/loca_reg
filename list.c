00300106ccf601cabe95dd7a54e18ae7db1ca1d5
Fatece <Fatece>
2016-06-08 20:55:08 -0300

    Functions cria,insere e exclui implemented

--- list.c
+++ list.c
@@ -1,16 +1,39 @@
 HEAD_LIST *cria_lista(char *genero)
 {
-	
+	HEAD_LIST *cabeca = NULL;
+	cabeca = (HEAD_LIST *) malloc(sizeof(HEAD_LIST));
+	if(cabeca == NULL)
+	{
+		printf("NAO FOI POSSIVEL ALOCA A CABECA DA LISTA!\n");
+		return NULL;
+	}	
+	return cabeca;
 }
 
-HEAD_LIST *insere_comeco(HEAD_LIST *comeco)
+int insere_comeco(HEAD_LIST *comeco, GAME *g)
 {
-	
+	LIST *new_node = NULL;
+	new_node = (LIST *) malloc(sizeof(LIST));
+	if(new_node == NULL)
+	{
+		printf("NAO FOI POSSIVEL ALOCAR UM NO!\n");
+		return -1;
+	}
+	new_node->prox = comeco->ptr_list;
+	comeco->ptr_list = new_node;
+	return 0;
 }
 
 void exclui_lista(HEAD_LIST *comeco)
 {
-	
+	LIST *aux = NULL, *aux2 = NULL;
+	for(aux = comeco->ptr_list; aux != NULL; )
+	{
+		aux2 = aux->prox;
+		free(aux);
+		aux = aux2;
+	}
+	free(comeco);
 }
 
 void insere_fim(HEAD_LIST *comeco)
