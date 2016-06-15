8c2658b2483676951475ac9557ab32d70c8e6f10
paulorjmx <pauloricardojm@hotmail.com.br>
2016-06-14 23:43:22 -0300

    Added a function to print list

--- inc/list.h
+++ inc/list.h
@@ -2,21 +2,22 @@
     #define LIST_H
     #include "loc.h"
 
-	typedef struct list 
+	typedef struct list
 	{
 		GAME *g;
 		struct list *prox;
 	}LIST;
-		
+
     typedef struct head_list
     {
-		char genero[31];
+		char *genero;
         LIST *ptr_list;
     } HEAD_LIST;
-	
-	
+
+
 	HEAD_LIST *cria_lista(char *genero); // Função que cria a lista;
 	int insere_comeco(HEAD_LIST *comeco, GAME *g); // Função para inserir um nó no começo;
 	void exclui_lista(HEAD_LIST *comeco); // Função que exclui a lista passado o começo dela;
+    void imp_lista(HEAD_LIST *comeco); // Função para imprimir todos os nós da lista
 	void insere_fim(HEAD_LIST *comeco); // Função para inserir um nó no começo;
 #endif
