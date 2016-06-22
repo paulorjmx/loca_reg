0140ba934195f3606aef83f49cbf81e5675b5c67
Fatece <Fatece>
2016-06-22 20:10:12 -0300

    Modified list to singly list without head

--- inc/list.h
+++ inc/list.h
@@ -8,16 +8,9 @@
 		struct list *prox;
 	}LIST;
 
-    typedef struct head_list
-    {
-		char *genero;
-        LIST *ptr_list;
-    } HEAD_LIST;
-
-
-	HEAD_LIST *cria_lista(char *genero); // Função que cria a lista;
-	int insere_comeco(HEAD_LIST *comeco, GAME *g); // Função para inserir um nó no começo;
-	void exclui_lista(HEAD_LIST *comeco); // Função que exclui a lista passado o começo dela;
-    void imp_lista(HEAD_LIST *comeco); // Função para imprimir todos os nós da lista
-	void insere_fim(HEAD_LIST *comeco); // Função para inserir um nó no começo;
+	LIST *cria_lista(GAME *g); // Função que cria a lista;
+	int insere_comeco(LIST *comeco, GAME *g); // Função para inserir um nó no começo;
+	void exclui_lista(LIST *comeco); // Função que exclui a lista passado o começo dela;
+    void imp_lista(LIST *comeco); // Função para imprimir todos os nós da lista;
+	void insere_fim(LIST *comeco, GAME *g); // Função para inserir um nó no fim;
 #endif
